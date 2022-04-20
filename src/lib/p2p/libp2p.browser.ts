25a26df7122fe4e6da96290a8ab03dbd82b5d8a3
chengcheng <chengcheng@qcraft.ai>
2022-04-20 13:50:37 +0800

    add some libp2p but cannot use

---
+++ src/lib/p2p/libp2p.browser.ts
@@ -0,0 +1,84 @@
+
+import { createLibp2p } from 'libp2p';
+// @ts-expect-error - no types
+import WS from 'libp2p-websockets'
+// @ts-expect-error - no types
+import WebRTCStar from 'libp2p-webrtc-star'
+// @ts-expect-error - no types
+import Multiplex from 'libp2p-mplex'
+import { NOISE } from '@chainsafe/libp2p-noise'
+import KadDHT from 'libp2p-kad-dht'
+import GossipSub from 'libp2p-gossipsub'
+import { validator, selector } from './ipns'
+
+export class Network {
+  private libp2p: any
+
+  genlibp2p = async () => {
+    this.libp2p = createLibp2p
+  }
+} 
+
+export function libp2pConfig () {
+  const options = {
+    dialer: {
+      maxParallelDials: 150, // 150 total parallel multiaddr dials
+      maxDialsPerPeer: 4, // Allow 4 multiaddrs to be dialed per peer in parallel
+      dialTimeout: 10e3 // 10 second dial timeout per peer dial
+    },
+    modules: {
+      transport: [
+        WS,
+        WebRTCStar
+      ],
+      streamMuxer: [
+        Multiplex
+      ],
+      connEncryption: [
+        NOISE
+      ],
+      peerDiscovery: [],
+      dht: KadDHT,
+      pubsub: GossipSub
+    },
+    config: {
+      peerDiscovery: {
+        autoDial: true,
+        // [Bootstrap.tag] = 'bootstrap'
+        bootstrap: {
+          enabled: true
+        },
+        // [WebRTCStar.discovery.tag]
+        webRTCStar: {
+          enabled: true
+        }
+      },
+      dht: {
+        kBucketSize: 20,
+        enabled: true,
+        clientMode: true,
+        // validators: {
+        //   ipns: validator
+        // },
+        // selectors: {
+        //   ipns: selector
+        // }
+      },
+      pubsub: {
+        enabled: true,
+        emitSelf: true
+      },
+      nat: {
+        enabled: false
+      }
+    },
+    metrics: {
+      enabled: true
+    },
+    peerStore: {
+      persistence: true
+    }
+  }
+
+  return options
+}
