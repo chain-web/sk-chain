1a985bb412501e6f532c6189d5f58f8637b66237
chengcheng <chengcheng@qcraft.ai>
2022-05-09 21:01:02 +0800

    add bytes export

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ export { skCacheKeys } from './lib/ipfs/key';
 export { Block } from './mate/block';
 export { Account } from './mate/account';
 export { Transaction } from './mate/transaction';
-export { CID } from 'multiformats';
+export { CID, bytes } from 'multiformats';
 export type { SkJsrInterface as SkGlobal } from './global';
 export interface CreateNodeConfig {
   // 网络id
