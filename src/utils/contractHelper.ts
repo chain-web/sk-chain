22829216cadbf33d027693bfd5e4398453c15df6
chengcheng <chengcheng@qcraft.ai>
2022-05-15 22:37:10 +0800

    implement simple SliceDb

--- src/utils/contractHelper.ts
+++ src/utils/contractHelper.ts
@@ -3,15 +3,28 @@ export type KeyType = 'base58' | 'base32';
 export namespace ConstractHelper {
   export type SliceDb<T> = {
     get: (key: string) => T;
+    has: (key: string) => boolean;
     delete: (key: string) => void;
     set: (key: string, value: T) => void;
   };
 }
 
+class SliceDb<T> implements ConstractHelper.SliceDb<T> {
+  constructor(keyType: KeyType) {
+    this.keyType = keyType;
+  }
+  // TODO 用keyType，用来做分片存储
+  keyType: KeyType;
+  db: Map<string, T> = new Map();
+
+  get = this.db.get as ConstractHelper.SliceDb<T>['get'];
+  set = this.db.set;
+  has = this.db.has;
+  delete = this.db.delete;
+}
+
 const createSliceDb = <T = any>(keyType: KeyType) => {
-  // 关联一个ipld实例到合约，用来做分片存储
-  sk.createDb(keyType);
-  return new Map() as ConstractHelper.SliceDb<T>;
+  return new SliceDb<T>(keyType);
 };
 
 const hash = (str: string) => {
