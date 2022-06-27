96ce1bc1fef62232ca7467d9644b65efce0380c9
chengcheng <chengcheng@qcraft.ai>
2022-06-27 16:55:16 +0800

    ed25519 to x25519 done

--- src/lib/p2p/did.ts
+++ src/lib/p2p/did.ts
@@ -1,40 +1,34 @@
 import { create, createFromPrivKey, createFromB58String } from 'peer-id';
 import { bytes } from 'multiformats';
-import { box } from 'tweetnacl';
+import { randomBytes, secretbox } from 'tweetnacl';
 import { message } from '../../utils/message';
 import { base58btc } from 'multiformats/bases/base58';
-import { concat } from 'uint8arrays/concat';
 import { __wbgtest_console_log } from 'cwjsr';
+import { fromString as uint8ArrayFromString } from 'uint8arrays/from-string';
+import * as lsm from 'libsodium-wrappers-sumo';
+
 export interface DidJson {
   id: string; // sk did
-  pubKey: string; // libp2p
+  pubKey?: string; // libp2p
   privKey: string; // libp2p
-  edPriv: string; // ed25519
-  edPub: string; // ed25519
 }
 
-const prefix_SKw3 = new Uint8Array([193, 149, 185, 110]);
-const prefix_D7 = new Uint8Array([9, 60]);
-const prefix_peerId = new Uint8Array([0, 36, 8, 1, 18, 32]);
+// https://libsodium.gitbook.io/doc/advanced/ed25519-curve25519
+// https://github.com/paulmillr/noble-ed25519#getsharedsecretprivatekey-publickey
+
+// const prefix_SKw3 = new Uint8Array([193, 149, 185, 110]);
+// const prefix_D7 = new Uint8Array([9, 60]);
+// const prefix_peerId = new Uint8Array([0, 36, 8, 1, 18, 32]);
 
 // 生成 libp2p de did
 export const genetateDid = async (): Promise<DidJson> => {
   const did = await create({ keyType: 'Ed25519' });
   // message.info(did.toJSON(), did);
   const simpleDid = did.toJSON();
-  const concatedKey = did.privKey.bytes; // bytes 是 key, priv, pub 的拼接
-  const boxPubId = box.keyPair.fromSecretKey(
-    concatedKey.slice(4, 36),
-  ).publicKey;
-  const id = concat([prefix_SKw3, concatedKey.slice(36), boxPubId]);
-  const edPriv = bytes.toHex(concatedKey.slice(4, 36));
-  const edPub = bytes.toHex(concatedKey.slice(36));
   const didJson: DidJson = {
-    id: base58btc.encode(id).substring(1), // remove first z
+    id: simpleDid.id, // remove first z
     privKey: simpleDid.privKey!,
     pubKey: simpleDid.pubKey!,
-    edPriv,
-    edPub,
   };
   return didJson;
 };
@@ -62,71 +56,45 @@ export const verifyById = async (
   return verifyed;
 };
 
+// 从libp2p私钥中解出ed priv
+export const privKeyToEdPriv = (priv: string) => {
+  const byte = uint8ArrayFromString(priv, 'base64pad');
+  return byte.slice(4);
+};
+
 // 解析出nacl public key
-export const parseBoxPubKey = (bid: string) => {
-  const byte = base58btc.decode(`z${bid}`);
-  return byte.slice(2);
-};
-
-// 从 did 解析出libp2p peerID 和 nacl public id
-export const parseSKDid = (did: string) => {
-  if (did.substring(0, 4) !== 'SKw3') {
-    throw new Error(`invalid did: ${did}`);
-  }
+export const parseBoxPubKey = (did: string) => {
   const byte = base58btc.decode(`z${did}`);
-  const pubKey = byte.slice(4, 36);
-  const peerId = concat([prefix_peerId, pubKey]);
-  const b58PeerId = base58btc.encode(peerId).substring(1);
-  const bid = base58btc
-    .encode(concat([prefix_D7, byte.slice(36)]))
-    .substring(1);
-  return {
-    bid,
-    peerId: b58PeerId,
-  };
+  const pku8 = byte.slice(6);
+  const xpk = lsm.crypto_sign_ed25519_pk_to_curve25519(pku8);
+  return xpk;
 };
 
-// const newNonce = () => randomBytes(secretbox.nonceLength);
-(async () => {
-  // gen dids
-  // const dids: DidJson[] = []
-  // for (let index = 0; index < 5; index++) {
-  //   const did = await genetateDid();
-  //   dids.push(did)
-  // }
-  // console.log(JSON.stringify(dids))
+// 从ed priv 解析出nacl priv key
+export const parseBoxPrivKey = (edPriv: Uint8Array) => {
+  const xsk = lsm.crypto_sign_ed25519_sk_to_curve25519(edPriv);
+  return xsk;
+};
 
-  // keys, messages & other inputs can be Uint8Arrays or hex strings
-  // Uint8Array.from([0xde, 0xad, 0xbe, 0xef]) === 'deadbeef'
+export const newNonce = () => randomBytes(secretbox.nonceLength);
+(async () => {
+  // await lsm.ready;
+  // // 生成did
   // const did = await genetateDid();
-  // const addr = parseSKDid(did.id);
-  // console.log(addr);
-  // console.log(did, did.id.length)
   // const did2 = await genetateDid();
-  // const privU8a = bytes.fromHex(did.edPriv);
-  // const pubU8a = bytes.fromHex(did.edPub);
-  // const message = bytes.fromString('cc nacl test');
-  // const publicKey = await ed.getPublicKey(privU8a);
-  // // console.log('publicKey', publicKey);
-  // const x25519 = await ed.getSharedSecret(privU8a, pubU8a);
-  // console.log('x25519', x25519);
-  // // const signature = await ed.sign(message, privateKey);
-  // // const isValid = await ed.verify(signature, message, publicKey);
-  // const bk = box.keyPair.fromSecretKey(privU8a);
-  // console.log(bk)
-  // const bk2 = box.keyPair.fromSecretKey(bytes.fromHex(did2.edPriv));
-  // console.log('did',did,bytes.toHex(bk.publicKey), bytes.toHex(bk.secretKey));
-  // console.log('did2',did2,bk2,bytes.toHex(bk2.publicKey), bytes.toHex(bk2.secretKey));
-  // const nonce = newNonce();
-  // const boxed = box(
-  //   message,
-  //   nonce,
-  //   bk2.publicKey,
-  //   bk.secretKey,
-  // );
-  // console.log('boxed', boxed)
-  // const unboxed = box.open(boxed, nonce, bk.publicKey, bk2.secretKey)
-  // console.log('unboxed', bytes.toString(unboxed!))
+  // const priv = privKeyToEdPriv(did.privKey);
+  // const priv2 = privKeyToEdPriv(did2.privKey);
+  // // 计算x25519 pk sk
+  // const xsk = parseBoxPrivKey(priv);
+  // const xsk2 = parseBoxPrivKey(priv2);
+  // const xpk = parseBoxPubKey(did.id);
+  // const xpk2 = parseBoxPubKey(did2.id);
+  // // 加密、解密
+  // const msg = 'test-msg'
+  // const nc = newNonce();
+  // const box = lsm.crypto_box_easy(msg, nc, xpk2, xsk);
+  // const unboxed = lsm.crypto_box_open_easy(box, nc, xpk, xsk2);
+  // console.log('encode-decode succ: ', bytes.toString(unboxed) === msg);
 })();
 
 // genetateDid();
