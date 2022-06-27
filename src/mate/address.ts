a4647f8eb88110e29de47b4e4f361fc64a7a58c5
chengcheng <chengcheng@qcraft.ai>
2022-06-27 16:58:44 +0800

    back address

--- src/mate/address.ts
+++ src/mate/address.ts
@@ -1,13 +1,6 @@
-import { parseSKDid } from '../lib/p2p/did';
-
 export class Address {
   constructor(address: string) {
-    const skdid = parseSKDid(address);
     this.did = address;
-    this.peerid = skdid.peerId;
-    this.bid = skdid.bid;
   }
-  did: string; // sk did
-  peerid: string; // libp2p peerid
-  bid: string; // nacl box public id
+  did: string;
 }
